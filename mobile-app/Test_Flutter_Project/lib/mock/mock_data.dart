286b28668a08719efc578391c470ddee7944f9cb
Wooshan Gamage <111788299+WooshanGamage@users.noreply.github.com>
2026-03-22 16:34:37 +0530

    Remove mock patient and vitals generation
    Removed mock patient data and vitals generation function.

--- mobile-app/Test_Flutter_Project/lib/mock/mock_data.dart
+++ mobile-app/Test_Flutter_Project/lib/mock/mock_data.dart
@@ -1,23 +1,3 @@
 import '../models/patient_model.dart';
 import '../models/vitals_model.dart';
 import 'dart:math';
-
-Patient mockPatient = Patient(
-  patientId: 'PT001',
-  fullName: 'John Silva',
-  gender: 'Male',
-  nic: '200112345678',
-  roomNumber: 'Ward 3 - Bed 12',
-  doctor: 'Dr. Perera',
-);
-
-Vitals generateMockVitals() {
-  final random = Random();
-  return Vitals(
-    spo2: 95 + random.nextInt(4),
-    pulse: 70 + random.nextInt(10),
-    temperature: 36.5 + random.nextDouble(),
-    lastUpdated: DateTime.now().toString(),
-  );
-}
-
