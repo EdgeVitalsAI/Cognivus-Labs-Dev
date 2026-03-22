9f1991736f24f1d1af840b4e7a77cf7619561f65
Wooshan Gamage <111788299+WooshanGamage@users.noreply.github.com>
2026-03-22 16:36:43 +0530

    Add mock patient data for testing

--- mobile-app/Test_Flutter_Project/lib/mock/mock_data.dart
+++ mobile-app/Test_Flutter_Project/lib/mock/mock_data.dart
@@ -1,3 +1,12 @@
 import '../models/patient_model.dart';
 import '../models/vitals_model.dart';
 import 'dart:math';
+
+Patient mockPatient = Patient(
+  patientId: 'PT004',
+  fullName: 'Dinithi Kavindya Rathnayake',
+  gender: 'Female',
+  nic: '200045678912',
+  roomNumber: 'Ward 2 - Bed 07',
+  doctor: 'Dr. Sanduni Jayawardena',
+);
