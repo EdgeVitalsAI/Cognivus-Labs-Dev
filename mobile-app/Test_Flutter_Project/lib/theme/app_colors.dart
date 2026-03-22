90e398851b55d24aab4a6c7e70514470d5301f60
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2026-03-22 17:51:59 +0530

    remove items and fix

--- mobile-app/Test_Flutter_Project/lib/theme/app_colors.dart
+++ mobile-app/Test_Flutter_Project/lib/theme/app_colors.dart
@@ -19,9 +19,7 @@ class AppColors {
   static const Color accentCyan = Color(0xFF39C5CF);
   
   // Status Colors
-  static const Color critical = Color(0xFFFF4444);
-  static const Color criticalDark = Color(0xFFCC0000);
-  static const Color warning = Color(0xFFFFAA00);
+
   static const Color warningLight = Color(0xFFFFCC00);
   static const Color success = Color(0xFF4CAF50);
   static const Color successLight = Color(0xFF66BB6A);
